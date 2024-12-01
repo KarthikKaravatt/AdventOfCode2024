9265813bb2871b2d61c278cb4c69740d7053b96a
KK <82811712+KarthikKaravatt@users.noreply.github.com>
2024-12-01 15:49:53 +0800

    Completed Day 1

---
+++ Advent/Day1/Day1.cs
@@ -0,0 +1,59 @@
+using System.IO;
+namespace Day1
+{
+    public static class Solution
+    {
+
+        public static int GetDistance(string filePath)
+        {
+            var (leftList, rightList) = ParseInput(filePath);
+            leftList.Sort();
+            rightList.Sort();
+            return leftList.Zip(rightList, (left, right) => Math.Abs(left-right)).Sum();
+        }
+        
+        public static int GetSimilarityScore(string filePath){
+            var (leftList, rightList) = ParseInput(filePath);
+            var similarity = 0;
+            leftList.ForEach(num => {
+                similarity+= num * rightList.FindAll(curNum => curNum == num).Count;
+            });
+            return similarity;
+        }
+
+        private static (List<int>, List<int>) ParseInput(string filePath)
+        {
+            List<int> leftList = new();
+            List<int> rightList = new();
+            string? line = "";
+            try
+            {
+                StreamReader streamReader = new StreamReader(filePath);
+                line = streamReader.ReadLine();
+                while (line != null)
+                {
+                    if (line != null)
+                    {
+                        var splitLine = line.Split("   ");
+                        if (int.TryParse(splitLine[0], out int num1))
+                        {
+                            leftList.Add(num1);
+                        }
+                        if (int.TryParse(splitLine[1], out int num2))
+                        {
+                            rightList.Add(num2);
+                        }
+                        line = streamReader.ReadLine();
+
+                    }
+                }
+                streamReader.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error reading the file" + e.Message);
+            }
+            return (leftList, rightList);
+        }
+    }
+}
--- Advent/Program.cs
+++ Advent/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day1;
+
+namespace Advent
+{
+    class Program
+    {
+        // The Main method is the entry point of the application
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(Day1.Solution.GetDistance("./Day1/input"));
+            // Console.WriteLine(Day1.Solution.GetSimilarityScore("./Day1/input"));
+
+        }
+    }
+}
